548179da0b76d95d56e01514f44656bf219f8b82
hayfa safa <haifasafaa@gmail.com>
2026-03-10 15:42:49 +0100

    add folder data.js , the information temporary for now

---
+++ my-journey-app/data.js
@@ -0,0 +1,71 @@
+export const MyJourney = 
+[
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Egypt", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Spain", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Tunis", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Swissland", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "USA", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Sweden ", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Bareclona",
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "lazpone", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Bruxceel ", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "paris", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+    img: "./src/assets/egypt.JPG" ,
+    city: "Marsiel", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  }
+
+ 
+]
