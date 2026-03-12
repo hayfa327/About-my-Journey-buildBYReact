935478b9665aefefff4f18090b2d2ea57e1e5d9c
hayfa safa <haifasafaa@gmail.com>
2026-03-12 15:17:57 +0100

    add .map

--- my-journey-app/data.js
+++ my-journey-app/data.js
@@ -1,71 +1,90 @@
 export const MyJourney = 
 [
   {
+    id: 1,
     img: "./src/assets/egypt.JPG" ,
-    city: "Egypt", 
+    city: "Cairo", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 2,
     img: "./src/assets/egypt.JPG" ,
-    city: "Spain", 
+    city: "Madrid", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 3,
     img: "./src/assets/egypt.JPG" ,
     city: "Tunis", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 4,
     img: "./src/assets/egypt.JPG" ,
     city: "Swissland", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 5,
     img: "./src/assets/egypt.JPG" ,
     city: "USA", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 6,
     img: "./src/assets/egypt.JPG" ,
     city: "Sweden ", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 7,
     img: "./src/assets/egypt.JPG" ,
     city: "Bareclona",
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 8,
     img: "./src/assets/egypt.JPG" ,
     city: "lazpone", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 9,
     img: "./src/assets/egypt.JPG" ,
     city: "Bruxceel ", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 10,
     img: "./src/assets/egypt.JPG" ,
     city: "paris", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   },
   {
+      id: 11,
+    img: "./src/assets/egypt.JPG" ,
+    city: "Marsiel", 
+    date: "July 2016" , 
+    description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
+  },
+  {
+      id: 12,
     img: "./src/assets/egypt.JPG" ,
     city: "Marsiel", 
     date: "July 2016" , 
     description : "I was in the south of Egypt, in Tunis Al-Fayoum, for a documentary movie workshop. During this workshop, I worked with local children to create a short film called 'Flour'."
   }
+
 
  
 ]
--- my-journey-app/src/App.jsx
+++ my-journey-app/src/App.jsx
@@ -1,10 +1,36 @@
+import { MyJourney } from "../data"
 import JourneyCard from "../journeyCard"
-
+ 
 
 export default function App() {
-  return (
-    <main>
-< JourneyCard />
-    </main>
-  )
+const journeyElements = MyJourney.map((journey) => {
+ return (
+    < JourneyCard 
+    img={journey.img}
+    city={journey.city}
+    date={journey.date}
+    description={journey.description}
+    /> 
+ )
+})
+return (
+<main>
+  {journeyElements}
+</main>
+)
+  
 }
+
+/* Where your project can go next
+
+Your My Journey app can become a very nice portfolio project if you later add:
+
+timeline of your life
+
+map of the journeys
+
+add new journey form
+
+filtering by country
+
+gallery view */
