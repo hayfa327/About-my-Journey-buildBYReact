fc00f7e2650f3701a927fac35338b2696f4cfb73
hayfa safa <haifasafaa@gmail.com>
2026-03-13 10:57:26 +0100

    create frontend folder

---
+++ my-journey-app/frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+import { MyJourney } from "./data"
+import JourneyCard from "./journeyCard"
+ 
+
+export default function App() {
+const journeyElements = MyJourney.map((journey) => {
+ return (
+    < JourneyCard 
+    key={journey.city + journey.date}
+    img={journey.img}
+    city={journey.city}
+    date={journey.date}
+    description={journey.description}
+    /> 
+ )
+})
+return (
+<main>
+  {journeyElements}
+</main>
+
+)
+}
+
+/* Where your project can go next
+
+Your My Journey app can become a very nice portfolio project if you later add:
+
+timeline of your life
+
+map of the journeys
+
+add new journey form
+
+filtering by country
+
+gallery view */
